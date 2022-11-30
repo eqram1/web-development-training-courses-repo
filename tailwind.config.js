a412f72a6229057832ed835d8cb5730671e76c6c
eqram1 <abmeqramhossain@gmail.com>
2022-11-30 07:58:50 +0600

    added theme generator in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: [
+      {
+        reactcourses: {
+          primary: '#0FCFEC',
+          secondary: '#0FCFEC',
+          accent: '#3A4256',
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
   theme: {
     extend: {},
   },
