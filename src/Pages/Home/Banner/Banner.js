6b4bfd7225cd7bdc7eb76214e3aa97666b1314c0
eqram1 <abmeqramhossain@gmail.com>
2022-11-30 08:27:58 +0600

    added banner section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import tutorial from '../../../assets/images/tutorial.jpeg';
 const Banner = () => {
     return (
-        <div className="hero bg-base-200">
+        <div className="hero">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" alt="" />
+                <img src={tutorial} className="rounded-lg w-1/2 shadow-2xl" alt="" />
                 <div>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold">React Tutorial News!</h1>
+                    <p className="py-6">In this React Tutorial,We are commited to everyone for best trainning from our best.From previous succes histories we are prouded.We wish your bright furure.</p>
                     <button className="btn btn-primary">Get premium access </button>
                 </div>
             </div>
