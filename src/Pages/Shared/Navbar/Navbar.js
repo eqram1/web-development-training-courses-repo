a74cc4b5842af514ff70fb34ca53c13a23a93004
eqram1 <abmeqramhossain@gmail.com>
2022-11-30 07:25:57 +0600

    added logo in navbar

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from '../../../assets/icons/logo.svg';
 const Navbar = () => {
 
     const menuItems = <React.Fragment>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/courses">Courses</Link></li>
+        <li><Link to="/">Courses</Link></li>
         <li><Link to="/faq">FAQ</Link></li>
         <li><Link to="/admission">Admission</Link></li>
         <li><Link to="/blog">Blog</Link></li>
@@ -17,13 +16,14 @@ const Navbar = () => {
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" alt="" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        <svg xmlns="" alt="" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         {menuItems}
                     </ul>
                 </div>
                 <Link className="btn btn-ghost normal-case text-xl">React Tutorial</Link>
+                <img src={logo} alt="" />
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
@@ -31,7 +31,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <Link className="btn">Get Premium Access</Link>
+                <Link className="btn">Courses Fee</Link>
             </div>
         </div>
     );
