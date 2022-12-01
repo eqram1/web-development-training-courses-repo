d15f5e33caf27447a0dc07c6e99ef9a7bf36a722
eqram1 <abmeqramhossain@gmail.com>
2022-12-01 18:05:44 +0600

    added footer section

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is footer</h2>
-        </div>
+        <footer className="p-10 ">
+            <div className='footer'>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link to="/" className="link link-hover">Subject</Link>
+                    <Link to="/" className="link link-hover">Admission</Link>
+                    <Link to="/" className="link link-hover">Sessions</Link>
+                    <Link to="/" className="link link-hover">Course fees</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link to="/" className="link link-hover">About us</Link>
+                    <Link to="/" className="link link-hover">Contact</Link>
+                    <Link to="/" className="link link-hover">Reviews</Link>
+                    <Link to="/" className="link link-hover">Public comments</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link to="/" className="link link-hover">Terms of admission</Link>
+                    <Link to="/" className="link link-hover">Privacy policy</Link>
+                    <Link to="/" className="link link-hover">Payment policy</Link>
+                </div>
+            </div>
+            <div className='text-center mt-32'>
+                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+            </div>
+        </footer>
     );
 };
 
